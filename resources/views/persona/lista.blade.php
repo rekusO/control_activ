c0440c7b10a8752c68bc2d1b7dc300e9b9e7e0e1
rekusO <tercoz12@hotmail.com>
2017-07-26 14:30:29 -0500

    efdef
    fdfd

--- resources/views/persona/lista.blade.php
+++ resources/views/persona/lista.blade.php
@@ -30,7 +30,6 @@
             <th>codigo</th>
             <th>Nombre</th>
             <th>Distrito</th>
-            <th>Distrito</th>
             <th>Apellido Paterno</th>
             <th>Apellido Materno</th>
             <th>DNI</th>
